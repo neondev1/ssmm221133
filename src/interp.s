04cdc7f65ca2a28ff0927bef4909bca7f3693651
neondev1 <82016241+neondev1@users.noreply.github.com>
2026-08-03 22:03:12 -0700

    Actually add interp.s

--- src/interp.s
+++ src/interp.s
@@ -24,7 +24,7 @@ loop:
 	mov	r0,		r1
 	not	r1
 	inc	r1				# r1 = -address
-	bgt	r1,		safe		# if high bit set, no overflow
+	bgt	r1,		safe		# if address high bit set, no overflow
 	halt					# if overflow, halt
 safe:
 	ld	$0x2,		r1
@@ -39,10 +39,12 @@ pad:
 	dec	r0
 	dec	r0
 	ld	(r0),		r1		# load instruction into lower 2 bytes of r0
-	ld	$0xffff,	r2
-	and	r2,		r1		# extract lower 2 bytes
 	inca	r0
 fetched:
+	ld	$0xffff,	r2
+	and	r2,		r1		# clean up junk in upper 2 bytes of r1
+	st	r0,		(r4)		# store opcode/op_imm
+
 	gpc	$0x6,		r6
 	j	decode
 	br	loop
@@ -51,14 +53,58 @@ fetched:
 	inca	r5
 	j	(r6)				# return, this will probably never happen
 
-# Decode and execute the instruction, given the pc in r0 and opcode in r1
+
+
+# Decode and execute the instruction, given the opcode in r1
 decode:
 	deca	r5
 	st	r6,		(r5)
 
 	mov	r1,		r2
-	shr	$0x4,		r2		# r2 = opcode
+	shr	$0xc,		r2		# r2 = opcode
 	shl	$0x3,		r2		# r2 = 8 * opcode
+	ld	$break,		r6
+	gpc	$0x4,		r0
+	add	r2,		r0		# calculate jump destination
+	j	(r0)
+
+	j	ld_imm				# 0
+	nop
+	j	ld				# 1
+	nop
+	j	ld_i				# 2
+	nop
+	j	st				# 3
+	nop
+	j	st_i				# 4
+	nop
+	.long	0xf0f0f0f0			# 5
+	.long	0xf0f0f0f0
+	j	alu				# 6
+	nop
+	j	sh				# 7
+	nop
+	j	br				# 8
+	nop
+	j	beq				# 9
+	nop
+	j	bgt				# a
+	nop
+	j	jmp				# b
+	nop
+	j	j_ind				# c
+	nop
+	j	j_d				# d
+	nop
+	j	j_d_i				# e
+	nop
+	j	sys				# f
+
+break:
+	ld	(r5),		r6
+	inca	r5
+	j	(r6)
+
 
 
 .pos	0x1fd0
@@ -79,3 +125,4 @@ r7:	.long	0x00000000
 
 .pos	0x2000
 vmem:	.long	0x00000000
+
