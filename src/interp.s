e822b43ab503a6594aca8682e228f4858c66ea30
neondev1 <82016241+neondev1@users.noreply.github.com>
2026-08-06 23:52:32 -0700

    Fix incorrect registers

--- src/interp.s
+++ src/interp.s
@@ -1,12 +1,15 @@
 # SM213 machine code interpreter
 
+# For all procedures, assume that r0, r1, r2, r7 will be clobbered
+
 .pos	0x0100
 start:
-	ld	$stack_bottom,	r5
-	inca	r5
+	ld	$stack_bottom,	r5		# DO NOT inca r5
 	gpc	$0x6,		r6
 	j	main
 	halt
+
+
 
 main:
 	deca	r5
@@ -14,36 +17,37 @@ main:
 
 	ld	$vmem,		r3
 	ld	$pc,		r4
-	ld	$op,		r7
 loop:
 	ld	(r4),		r0		# r0 = pc
 	mov	r0,		r1
 	shr	$0x1f,		r1		# r1 = high bit of pc (0 or 0b1...1)
 	beq	r1,		safe		# if unset, no chance of overflow
-	add	r3,		r0		# r0 = actual instruction address
 	mov	r0,		r1
-	not	r1
-	inc	r1				# r1 = -address
-	bgt	r1,		safe		# if address high bit set, no overflow
+	add	r3,		r1		# r1 = actual instruction address
+	mov	r1,		r2
+	not	r2
+	inc	r2				# r2 = -address
+	bgt	r2,		safe		# if address high bit set, no overflow
 	halt					# if overflow, halt
 safe:
-	ld	$0x2,		r1
-	and	r0,		r1		# r1 = r0 & 0b...10
-	bgt	r1,		pad		# if not 4-byte-aligned, pad
-	ld	(r0),		r1		# load instruction into upper 2 bytes of r0
+	ld	$0x2,		r2
+	and	r1,		r2		# r2 = address & 0b...10
+	bgt	r2,		pad		# if not 4-byte-aligned, pad
+	ld	(r1),		r1		# load instruction into upper 2 bytes of r1
 	shr	$0x10,		r1		# extract upper 2 bytes
 	inc	r0
 	inc	r0
 	br	fetched
 pad:
-	dec	r0
-	dec	r0
-	ld	(r0),		r1		# load instruction into lower 2 bytes of r0
-	inca	r0
+	dec	r1
+	dec	r1
+	ld	(r1),		r1		# load instruction into lower 2 bytes of r1
+	inc	r0
+	inc	r0
 fetched:
 	ld	$0xffff,	r2
 	and	r2,		r1		# clean up junk in upper 2 bytes of r1
-	st	r0,		(r4)		# store opcode/op_imm
+	st	r0,		(r4)		# store pc
 
 	gpc	$0x6,		r6
 	j	decode
@@ -71,7 +75,20 @@ decode:
 
 
 
+# 0d--vvvvvvvv			ld	$v,		rd
+ld_imm:
+
+
+
+
+# 1psd	(p=o>>2)		ld	o(rs),		rd
+ld:
+
+
+
+
 .pos	0x1f00
+stack_bottom:
 halt:
 	halt			# for bad jumps
 jump_table:
@@ -93,7 +110,6 @@ jump_table:
 	.long	sys		# f
 
 .pos	0x1fd0
-stack_bottom:
 pc:	.long	0x00000000
 op:	.long	0x00000000
 op_ext:	.long	0x00000000
