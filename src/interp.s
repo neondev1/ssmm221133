89bb4d4463a67cee1990b3670ef54f997f654b78
neondev1 <82016241+neondev1@users.noreply.github.com>
2026-08-11 13:16:05 -0700

    Add procedure for loading 6-byte instructions

--- src/interp.s
+++ src/interp.s
@@ -4,7 +4,8 @@
 
 .pos	0x0100
 start:
-	ld	$stack_bottom,	r5		# DO NOT inca r5
+	ld	$stack_bottom,	r5
+	inca	r5
 	gpc	$0x6,		r6
 	j	main
 	halt
@@ -16,8 +17,8 @@ main:
 	st	r6,		(r5)
 
 loop:
-	ld	$pc,		r0
-	ld	(r0),		r0		# r0 = pc
+	ld	$pc,		r7
+	ld	(r7),		r0		# r0 = pc
 	mov	r0,		r1
 	shr	$0x1f,		r1		# r1 = high bit of pc (0 or 0b1...1)
 	beq	r1,		safe		# if unset, no chance of overflow
@@ -44,26 +45,26 @@ pad:
 	ld	(r1),		r2		# load instruction into lower 2 bytes of r2
 	inca	r1
 	ld	(r1),		r3		# load ins_op_ext as it is convenient here
+	ld	$op_ext,	r4
+	st	r3,		(r4)		# store in a global for now
 	inc	r0
 	inc	r0
 fetched:
 	ld	$0xffff,	r1
 	and	r1,		r2		# clean up junk in upper 2 bytes of r2
-	ld	$pc,		r1
-	st	r0,		(r1)		# store pc
+	st	r0,		(r7)		# store pc
 
 	mov	r2,		r1
 	shr	$0xc,		r1		# r1 = opcode
 
 	deca	r5
-	deca	r5
-	st	r2,		0x0(r5)
-	st	r3,		0x4(r5)
+	st	r2,		(r5)
 	ld	$table,		r0
 	gpc	$0x2,		r6
 	j	*(r0,r2,4)			# call procedure for specific opcode
 	inca	r5
-	inca	r5
+
+	br	loop
 
 	ld	(r5),		r6
 	inca	r5
@@ -71,18 +72,41 @@ fetched:
 
 
 
-# Load the last 4 bytes of a 6-byte instruction
+# Load the last 4 bytes of a 6-byte instruction and return it in r0; increment pc
 load_ext:
+	ld	$pc,		r7
+	ld	(r7),		r0
+	ld	$0x2,		r2
+	and	r0,		r2		# r2 = pc & 0b...10
+	ld	$op_ext,	r3
+	ld	(r3),		r3
+	beq	r2,		aligned		# if 4-byte-aligned, already loaded
+	ld	$mem,		r1
+	add	r0,		r1		# r1 = instruction address
+	dec	r1
+	dec	r1
+	ld	0x0(r1),	r3
+	shl	$0x10,		r3		# first 2 bytes
+	ld	0x4(r1),	r4
+	shr	$0x10,		r4
+	ld	$0xffff,	r2
+	and	r2,		r4		# next 2 bytes
+	add	r4,		r3		# add instead of bitwise or is fine here
+aligned:
+	inca	r0				# update pc
+	st	r0,		(r7)
+	mov	r3,		r0		# return all 4 bytes
+	j	(r6)
 
 
 
-
+# Load immediate
 # 0d--vvvvvvvv			ld	$v,		rd
 ld_imm:
 
 
 
-
+# Load base+offset
 # 1psd	(p=o>>2)		ld	o(rs),		rd
 ld:
 
@@ -91,6 +115,7 @@ ld:
 
 .pos	0x1f00
 stack_bottom:
+	.long	0x0
 halt:
 	halt			# for bad jumps
 table:
@@ -99,7 +124,7 @@ table:
 	.long	ld_i		# 2
 	.long	st		# 3
 	.long	st_i		# 4
-	.long	halt		# 5 (unused)
+	.long	halt		# 5 (unused, maybe use for xchg later)
 	.long	alu		# 6
 	.long	sh		# 7
 	.long	br		# 8
@@ -113,7 +138,6 @@ table:
 
 .pos	0x1fd0
 pc:	.long	0x00000000
-op:	.long	0x00000000
 op_ext:	.long	0x00000000
 
 .pos	0x1fe0
