9c3111efd24c15c5b4544343f4c60242dc21701f
neondev1 <82016241+neondev1@users.noreply.github.com>
2026-08-10 23:58:09 -0700

    Load ins_op_ext in fetch/decode cycle when convenient

--- src/interp.s
+++ src/interp.s
@@ -1,6 +1,6 @@
 # SM213 machine code interpreter
-
-# For all procedures, assume that r0-r4 will be clobbered
+# This module expects well-formed machine code (i.e. the output of sm2hex.s) as input.
+# As such, it performs minimal error checking (notably skipping register validity checks).
 
 .pos	0x0100
 start:
@@ -17,47 +17,63 @@ main:
 
 loop:
 	ld	$pc,		r0
-	ld	(r0),		r7		# r7 = pc
-	mov	r7,		r1
+	ld	(r0),		r0		# r0 = pc
+	mov	r0,		r1
 	shr	$0x1f,		r1		# r1 = high bit of pc (0 or 0b1...1)
 	beq	r1,		safe		# if unset, no chance of overflow
 	ld	$mem,		r1
-	add	r7,		r1		# r1 = actual instruction address
-	mov	r1,		r2
-	not	r2
-	inc	r2				# r2 = -address
-	bgt	r2,		safe		# if address high bit set, no overflow
+	add	r0,		r1		# r1 = actual instruction address
+	not	r1				# r1 = ~address
+	bgt	r1,		safe		# if address high bit set, no overflow
+	beq	r1,		safe
 	halt					# if overflow, halt
 safe:
+	ld	$mem,		r1
+	add	r0,		r1		# r1 = actual instruction address
 	ld	$0x2,		r2
 	and	r1,		r2		# r2 = address & 0b...10
 	bgt	r2,		pad		# if not 4-byte-aligned, pad
-	ld	(r1),		r1		# load instruction into upper 2 bytes of r1
-	shr	$0x10,		r1		# extract upper 2 bytes
-	inc	r7
-	inc	r7
+	ld	(r1),		r2		# load instruction into upper 2 bytes of r2
+	shr	$0x10,		r2		# extract upper 2 bytes
+	inc	r0
+	inc	r0
 	br	fetched
 pad:
 	dec	r1
 	dec	r1
-	ld	(r1),		r1		# load instruction into lower 2 bytes of r1
-	inc	r7
-	inc	r7
+	ld	(r1),		r2		# load instruction into lower 2 bytes of r2
+	inca	r1
+	ld	(r1),		r3		# load ins_op_ext as it is convenient here
+	inc	r0
+	inc	r0
 fetched:
-	ld	$0xffff,	r2
-	and	r2,		r1		# clean up junk in upper 2 bytes of r1
-	ld	$pc,		r0
-	st	r7,		(r0)		# store pc
+	ld	$0xffff,	r1
+	and	r1,		r2		# clean up junk in upper 2 bytes of r2
+	ld	$pc,		r1
+	st	r0,		(r1)		# store pc
 
-	mov	r1,		r2
-	shr	$0xc,		r2		# r2 = opcode
-	ld	$jump_table,	r0
+	mov	r2,		r1
+	shr	$0xc,		r1		# r1 = opcode
+
+	deca	r5
+	deca	r5
+	st	r2,		0x0(r5)
+	st	r3,		0x4(r5)
+	ld	$table,		r0
 	gpc	$0x2,		r6
-	j	*(r0,r2,4)
+	j	*(r0,r2,4)			# call procedure for specific opcode
+	inca	r5
+	inca	r5
 
 	ld	(r5),		r6
 	inca	r5
 	j	(r6)				# return, this will probably never happen
+
+
+
+# Load the last 4 bytes of a 6-byte instruction
+load_ext:
+
 
 
 
@@ -77,7 +93,7 @@ ld:
 stack_bottom:
 halt:
 	halt			# for bad jumps
-jump_table:
+table:
 	.long	ld_imm		# 0
 	.long	ld		# 1
 	.long	ld_i		# 2
@@ -101,14 +117,15 @@ op:	.long	0x00000000
 op_ext:	.long	0x00000000
 
 .pos	0x1fe0
-r0:	.long	0x00000000
-r1:	.long	0x00000000
-r2:	.long	0x00000000
-r3:	.long	0x00000000
-r4:	.long	0x00000000
-r5:	.long	0x00000000
-r6:	.long	0x00000000
-r7:	.long	0x00000000
+regs:
+	.long	0x00000000	# r0
+	.long	0x00000000	# r1
+	.long	0x00000000	# r2
+	.long	0x00000000	# r3
+	.long	0x00000000	# r4
+	.long	0x00000000	# r5
+	.long	0x00000000	# r6
+	.long	0x00000000	# r7
 
 .pos	0x2000
 vmem:	.long	0x00000000
